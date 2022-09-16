ea85fee05f0e3a75038b98aee945defcd2e7ae95
wahyuhjr <wahyuhidjr11@gmail.com>
2022-09-16 20:17:26 +0700

    slicing-2

---
+++ ctlg/resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <link href="{{  asset ('assets/css/home.css')  }}" rel="stylesheet" />
+    @vite(['resources/css/app.css', 'resources/js/app.js']) @vite('resources/js/app.js')
+    <link rel="stylesheet" href="{{ mix ('css/app.css') }}" />
+    <title>Home</title>
+</head>
+
+<body>
+    <!-- component -->
+    @include('layouts.navbar')
+    @yield('content')
+
+
+
+    <!-- end about -->
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
